327b00d5c5683929a77e5f8714e70ab9145c33e3
Christopher Karras <ckarras11@gmail.com>
2017-09-04 14:04:39 -0400

    Added post endpoint to server.js

--- server.js
+++ server.js
@@ -17,6 +17,20 @@ app.get('/blog-posts', (request, response) => {
     response.json(BlogPosts.get());
 });
 
+app.post('/blog-posts', jsonParser, (request, response) => {
+    const requiredFields = ['title', 'content', 'author'];
+    requiredFields.forEach((field, index) => {
+        field = requiredFields[index];
+        if(!(field in request.body)){
+            const message = `Post must include field: ${field}`
+            console.log(message);
+            response.status(400).send(message);
+        }   
+    })
+    const post = BlogPosts.create(request.body.title, request.body.content, request.body.author, request.body.date);
+    response.status(201).json(post);
+});
+
 
 
 
