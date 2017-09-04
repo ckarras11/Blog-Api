733c5c062b8adaaba15bfc9a82fdc21305d28680
Christopher Karras <ckarras11@gmail.com>
2017-09-04 14:11:37 -0400

    Added delete endpoint to server.js

--- server.js
+++ server.js
@@ -7,20 +7,29 @@ const {BlogPosts} = require('./models')
 const jsonParser = bodyParser.json();
 const app = express();
 
+// Initializes logger
+
 app.use(morgan('common'));
+
+// Creates intital blog posts
 
 BlogPosts.create('test', 'Dolor amet voluptate sint', 'Chris', Date.now())
 BlogPosts.create('test 2', 'This is another test', 'Chris', Date.now())
 BlogPosts.create('test 3', 'This is a third test', 'Chris', Date.now())
 
+// Get request
+
 app.get('/blog-posts', (request, response) => {
     response.json(BlogPosts.get());
 });
+
+// Post request
 
 app.post('/blog-posts', jsonParser, (request, response) => {
     const requiredFields = ['title', 'content', 'author'];
     requiredFields.forEach((field, index) => {
         field = requiredFields[index];
+        // Checks to make sure all required fields are entered
         if(!(field in request.body)){
             const message = `Post must include field: ${field}`
             console.log(message);
@@ -31,10 +40,15 @@ app.post('/blog-posts', jsonParser, (request, response) => {
     response.status(201).json(post);
 });
 
+// Delete request
+
+app.delete('/blog-posts/:id', (request, response) => {
+    BlogPosts.delete(request.params.id);
+    response.status(204).end();
+})
 
 
-
-
+// Initializes app listening on port 8080
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Your app is listening on port ${process.env.PORT || 8080}`)
 })
