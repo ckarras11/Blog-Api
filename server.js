073f03c5e65e8322c47aeeb3bf8b9ed235e64eaa
Christopher Karras <ckarras11@gmail.com>
2017-09-04 20:32:52 -0400

    Moved blog-post endpoints to seperate file with express router

--- server.js
+++ server.js
@@ -1,78 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 
-
-const {BlogPosts} = require('./models')
-const jsonParser = bodyParser.json();
+const blogPostsRouter = require('./blog-posts')
 const app = express();
 
 // Initializes logger
 
 app.use(morgan('common'));
 
-// Creates intital blog posts
 
-BlogPosts.create('test', 'Dolor amet voluptate sint', 'Chris', Date.now())
-BlogPosts.create('test 2', 'This is another test', 'Chris', Date.now())
-BlogPosts.create('test 3', 'This is a third test', 'Chris', Date.now())
-
-// Get request
-
-app.get('/blog-posts', (request, response) => {
-    response.json(BlogPosts.get());
-});
-
-// Post request
-
-app.post('/blog-posts', jsonParser, (request, response) => {
-    const requiredFields = ['title', 'content', 'author'];
-    requiredFields.forEach((field, index) => {
-        field = requiredFields[index];
-        // Checks to make sure all required fields are entered
-        if(!(field in request.body)){
-            const message = `Post must include field: ${field}`
-            console.log(message);
-            response.status(400).send(message);
-        }   
-    })
-    const post = BlogPosts.create(request.body.title, request.body.content, request.body.author, request.body.date);
-    response.status(201).json(post);
-});
-
-// Delete request
-
-app.delete('/blog-posts/:id', (request, response) => {
-    BlogPosts.delete(request.params.id);
-    response.status(204).end();
-});
-
-// Put request
-
-app.put('/blog-posts/:id', jsonParser, (request, response) => {
-    const requiredFields = ['title', 'content', 'author'];
-    requiredFields.forEach((field, index) => {
-        field = requiredFields[index];
-        if(!(field in request.body)){
-            const message = `Body must include field ${field}`
-            console.log(message);
-            response.status(400).send(message);
-        }
-    });
-    if(request.params.id !== request.body.id){
-        const message = `Params id ${request.params.id} must match body id ${request.body.id}`;
-        console.log(message);
-        response.status(400).send(message);
-    }
-    BlogPosts.update({
-        id: request.params.id,
-        title: request.body.title,
-        content: request.body.content,
-        author: request.body.author,
-        publishDate: request.body.date
-    });
-    response.status(204).end();
-});
+app.use('/blog-posts', blogPostsRouter);
 
 
 
