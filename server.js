ad0f5eb8270d7bfb2eaaf7067cc4a771991a963a
Christopher Karras <ckarras11@gmail.com>
2017-09-09 06:05:19 -0700

    Added testing to get and post endpoints

--- server.js
+++ server.js
@@ -11,9 +11,34 @@ app.use(morgan('common'));
 
 app.use('/blog-posts', blogPostsRouter);
 
+let server;
 
+function runServer() {
+    const port = process.env.PORT || 8080;
+    return new Promise((resolve, reject) => {
+        server = app.listen(port, () => {
+            console.log(`Your app is listening on port ${port}`);
+            resolve(server);
+        }).on('error', err => {
+            reject(err)
+        });
+    });
+}
 
-// Initializes app listening on port 8080
-app.listen(process.env.PORT || 8080, () => {
-    console.log(`Your app is listening on port ${process.env.PORT || 8080}`)
-})
+function closeServer() {
+    return new Promise((resolve, reject) => {
+        server.close(err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve();
+        });
+    });
+}
+
+if (require.main === module) {
+    runServer().catch(err => console.log(err));
+}
+
+module.exports = {app, runServer, closeServer};
---
+++ test/test-server.js
@@ -0,0 +1,62 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const { app, runServer, closeServer } = require('../server.js');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('blog-posts', function () {
+
+    before(function () {
+        return runServer;
+    });
+
+    after(function () {
+        return closeServer;
+    });
+
+    it('should list items on GET', function () {
+        return chai.request(app)
+            .get('/blog-posts')
+            .then(function (res) {
+                res.should.have.status(200);
+                res.body.length.should.be.at.least(1);
+                res.should.be.json;
+                res.body.should.be.a('array');
+            });
+    });
+
+    it('should add items on POST', function () {
+
+        const newPost = {
+            title: 'Chris',
+            content: 'dsojfasdijfisoajfdiosjfiosdjfasiod',
+            author: 'Cassie',
+        }
+        return chai.request(app)
+            .post('/blog-posts')
+            .send(newPost)
+            .then(function (res) {
+                res.should.have.status(201);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                console.log(Object.keys(res.body).length)
+                res.body.forEach(key => {
+                    res.body[key]
+                })
+                
+        })
+
+    })
+
+    it('should remove an item on DELETE', function () {
+
+    })
+
+    it('should update items on PUT', function () {
+
+    })
+
+})
